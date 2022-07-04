827678542cd2187bc9fa3148d77574993ac973c5
Jeongyoon Yang <backyang0716@gmail.com>
2022-07-04 16:49:30 +0900

    base complex class added

--- complex.py
+++ complex.py
@@ -11,6 +11,15 @@ class Complex:
 
     def __str__(self):
         return str(self.re) + "+" + str(self.im) + "i"
+        
+    def add(self, c1):
+        c = Complex()
+        c.re = self.re + c1.re
+        c.im = self.im + c1.im
+        return c
 
 c1 = Complex(1, 2)
 print(c1)
+
+c2 = Complex(2,3)
+print(c1.add(c1,add(c2))
